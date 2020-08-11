0d766e46ca76fc8b8ff834b23e9f448f36c910bb
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2020-08-10 22:45:15 -0400

    Initial Commit

---
+++ RWBlueLibrary/LibrairyAPI.swift
@@ -0,0 +1,17 @@
+//
+//  LibrairyAPI.swift
+//  RWBlueLibrary
+//
+//  Created by Helwan Mandé on 2020-08-10.
+//
+//
+
+import Foundation
+
+final class LibrairyAPI {
+  
+  static let shared = LibrairyAPI()
+  
+  private init() {}
+}
+
